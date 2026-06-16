78786368c5a22762a180f43ed60cabc4ab708eb0
sangamithra-18 <sangami.rengaraj1895@gmail.com>
2026-06-16 14:12:12 +0530

    Initial FastAPI project

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def home():
+  return {"message": "Hello World!THIS IS A TEST!"}  
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}    
