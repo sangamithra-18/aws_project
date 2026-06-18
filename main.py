111a5d01f35d71a7503eb7aa2140c50e0a03edad
sangamithra-18 <sangami.rengaraj1895@gmail.com>
2026-06-18 12:35:08 +0530

    test ci/cd

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def home():
-  return {"message": "Hello World!THIS IS A TEST!"
-  ""}  
+  return {"message": "Hello World!THIS IS A TEST! and cicd test is working fine"""}  
 
 @app.get("/health")
 def health():
-    return {"status": "healthy"}    
+    return {"status": "Healthy Healthy"}    
 
 
 @app.get("/home")
